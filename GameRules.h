5585d3d11431c67e91b14641e8d492b4109cd5da
Aylin Oymak <aylin.oymak@safetyio.com>
2025-03-01 21:39:08 +0100

    add comments

--- GameRules.h
+++ GameRules.h
@@ -4,14 +4,22 @@
 #include <SFML/Graphics.hpp>
 #include "Button.h"
 
+// This class represents the rules screen of the game
 class GameRules {
 public:
+    //Constructor
     explicit GameRules(const sf::Font& font);
 
+    // Handles user input events (e.g., mouse clicks)
     void handleEvent(const sf::Event& event, const sf::RenderWindow& window);
+
+    // Updates the state of the GameRules screen (e.g., button states)
     void update(const sf::RenderWindow& window);
+
+    // Draws the GameRules screen (background, text, and buttons) to the window
     void draw(sf::RenderWindow& window) const;
 
+    // Sets the callback function for the "Back" button click
     void setOnBackClicked(std::function<void()> onClick);
 
 private:
