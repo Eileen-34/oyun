b0d46690f7e3524ff8ccd8ce9da67c8c066ea555
Aylin Oymak <aylin.oymak@safetyio.com>
2025-03-01 21:36:09 +0100

    add comments

--- GameRules.cpp
+++ GameRules.cpp
@@ -1,6 +1,7 @@
 #include "GameRules.h"
 #include "rules.h" // Byte array for rules.png
 
+// Constructor
 GameRules::GameRules(const sf::Font& font)
         : backButton("Back", font, sf::Vector2f(120, 700), sf::Vector2f(100, 50)) {
     // Load the background image from memory
@@ -10,35 +11,38 @@ GameRules::GameRules(const sf::Font& font)
     backgroundSprite.setTexture(backgroundTexture);
 
     /*
-    // Load the background image
+    // Load the background image from file
     if (!backgroundTexture.loadFromFile("/Users/03oymaka19/CLionProjects/oyun/assets/rules.png")) {
         throw std::runtime_error("Failed to load rules background image!");
     }
     backgroundSprite.setTexture(backgroundTexture);
-*/
+    */
 
-    // Resize the background image to make it smaller
+    // Resizes the background image
     float scaleFactor = 0.6f;
     backgroundSprite.setScale(scaleFactor, scaleFactor);
 
-    // Position the background image at the top of the screen
+    // Positions the background image at the top of the screen
     float windowWidth = 800; // Replace with your window width
     float imageWidth = backgroundSprite.getGlobalBounds().width;
-    backgroundSprite.setPosition((windowWidth - imageWidth) / 2, 20); // 20 pixels from the top
+    backgroundSprite.setPosition((windowWidth - imageWidth) / 2, 20);
 
-    // Set up the "Game Rules" text
+    // Sets up the "Rules" text
     rulesText.setFont(font);
     rulesText.setString(
-            "The rules are pretty straightforward.\n"
-            "\n"
+            "The rules:\n"
             "1. Rock wins against scissors.\n"
             "2. Scissors win against paper.\n"
             "3. Paper wins against rock. \n"
+            "4. Round: Each game consists of five rounds.\n"
+            "5. Scoring:\n"
+            "- If it is a tie, neither player scores a point.\n"
+            "- Otherwise, the winner of each round earns one point."
     );
     rulesText.setCharacterSize(30);
     rulesText.setFillColor(sf::Color::White);
 
-    // Center the "Rules" text on the screen
+    // Centers the "Rules" text on the screen
     sf::FloatRect textBounds = rulesText.getLocalBounds();
     rulesText.setOrigin(textBounds.width / 2, textBounds.height / 2);
     rulesText.setPosition(120, 480); //
@@ -53,13 +57,13 @@ void GameRules::update(const sf::RenderWindow& window) {
 }
 
 void GameRules::draw(sf::RenderWindow& window) const {
-    // Draw the background
+    // Draws the background
     window.draw(backgroundSprite);
 
-    // Draw the "Game Rules" text
+    // Draws the "Rules" text
     window.draw(rulesText);
 
-    // Draw the "Back to Menu" button
+    // Draws the "Back" button
     backButton.draw(window);
 }
 
