9297ef8ab73326e8acc7aef9eda24c4da668fa4d
Aylin Oymak <aylin.oymak@safetyio.com>
2025-03-01 22:28:31 +0100

    remove scores from menu and add pragma once

--- Menu.cpp
+++ Menu.cpp
@@ -1,12 +1,12 @@
+#pragma once
 #include "Menu.h"
 
 Menu::Menu(const sf::Font& font) {
     // Create buttons
-    buttons.emplace_back("Play", font, sf::Vector2f(300, 300), sf::Vector2f(200, 50));      ///DONE
-    buttons.emplace_back("Scores", font, sf::Vector2f(300, 400), sf::Vector2f(200, 50));   ///TODO
-    buttons.emplace_back("Rules", font, sf::Vector2f(300, 500), sf::Vector2f(200, 50)); ///DONE
-    buttons.emplace_back("About", font, sf::Vector2f(300, 600), sf::Vector2f(200, 50));      ///DONE
-    buttons.emplace_back("Exit", font, sf::Vector2f(300, 700), sf::Vector2f(200, 50));       ///DONE
+    buttons.emplace_back("Play", font, sf::Vector2f(300, 300), sf::Vector2f(200, 50));
+    buttons.emplace_back("Rules", font, sf::Vector2f(300, 400), sf::Vector2f(200, 50));
+    buttons.emplace_back("About", font, sf::Vector2f(300, 500), sf::Vector2f(200, 50));
+    buttons.emplace_back("Exit", font, sf::Vector2f(300, 600), sf::Vector2f(200, 50));
 }
 
 void Menu::handleEvent(const sf::Event& event, const sf::RenderWindow& window) {
@@ -31,18 +31,14 @@ void Menu::setOnPlayClicked(std::function<void()> onClick) {
     buttons[0].setOnClick(onClick);
 }
 
-void Menu::setOnScoresClicked(std::function<void()> onClick) {
+void Menu::setOnRulesClicked(std::function<void()> onClick) {
     buttons[1].setOnClick(onClick);
 }
 
-void Menu::setOnRulesClicked(std::function<void()> onClick) {
+void Menu::setOnAboutClicked(std::function<void()> onClick) {
     buttons[2].setOnClick(onClick);
 }
 
-void Menu::setOnAboutClicked(std::function<void()> onClick) {
-    buttons[3].setOnClick(onClick);
-}
-
 void Menu::setOnExitClicked(std::function<void()> onClick) {
-    buttons[4].setOnClick(onClick);
+    buttons[3].setOnClick(onClick);
 }
